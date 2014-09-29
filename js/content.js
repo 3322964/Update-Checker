1638033e05137df0033ea97df10bce59893f72a9
lacroix-prime <prime.yann@gmail.com>
2014-09-29 09:15:45 +0200

    404 error pages management for content.js matches

--- js/content.js
+++ js/content.js
@@ -1,13 +1,13 @@
 var type;
 var value = document.URL.match(/^http:\/\/www.imdb.com\/title\/(tt[0-9]+)\/(?:\?.*)?$/);
-if (value != null) {
+if (value != null && document.title != '404 Error - IMDb') {
     type  = document.title.match(/\([^\)]*Series[^\)]*\)/) ? 'series' : 'movies';
     value = value[1];
     chrome.runtime.sendMessage({ 'setPopup': true });
 }
 else {
     value = document.URL.match(/^http:\/\/www.blu-ray.com\/movies\/([^\/]*\/[^\/]*\/)/);
-    if (value != null) {
+    if (value != null && document.title != '404 Not Found') {
         type  = 'blurays';
         value = value[1];
         chrome.runtime.sendMessage({ 'setPopup': true });
