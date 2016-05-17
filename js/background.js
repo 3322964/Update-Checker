9e6928ffc8708e2aa970a0541dbcde3654529dce
lacroix-prime <prime.yann@gmail.com>
2016-05-17 16:16:35 +0200

    Fixed background to new specs

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.query({ url: chrome.runtime.getURL('index.html'), windowId: tab.windowId }, function (tabs) {
+chrome.browserAction.onClicked.addListener(function () {
+    chrome.tabs.query({ url: chrome.runtime.getURL('index.html') }, function (tabs) {
         if (tabs.length == 0)
             window.open('index.html');
-        else chrome.tabs.update(tabs[0].id, { selected: true }, function () {});
+        else chrome.tabs.update(tabs[0].id, { highlighted: true });
     });
 });
